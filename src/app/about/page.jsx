0c9ac2bdcdb5dbada3dd32228672ed2d0eb4cff0
Simon Petrus <simon.petrus.jkt@gmail.com>
2024-05-11 09:21:59 +0700

    1:48:41 kelar halaman About

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,19 @@
 "use client";
 import Brain from "@/components/brain";
-import { motion, useScroll } from "framer-motion";
+import { motion, useInView, useScroll } from "framer-motion";
 import { useRef } from "react";
 
 const AboutPage = () => {
   const containerRef = useRef();
   const { scrollYProgress } = useScroll({ container: containerRef });
   // console.log(scrollYProgress); // angkanya antara 0-1
+
+  const skillRef = useRef();
+  // const isSkillRefInView = useInView(skillRef, { once: true }); // "once"kalau mau sekali muncul aja
+  const isSkillRefInView = useInView(skillRef, { margin: "-100px" }); // "margin" kalau mau animasinya muncul ketika udah elemennya udah masuk (discroll) 100px
+
+  const experienceRef = useRef();
+  const isExperienceInView = useInView(experienceRef, { margin: "-100px" });
 
   return (
     <motion.div
@@ -50,12 +57,46 @@ const AboutPage = () => {
                 />
               </svg>
             </div>
+            {/* SKILL SCROLL SVG */}
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
           </div>
           {/* SKILLS CONTAINER */}
-          <div className=" flex flex-col justify-center gap-12">
-            <h1 className=" text-2xl font-bold">SKILLS</h1>
+          <div className=" flex flex-col justify-center gap-12" ref={skillRef}>
+            <motion.h1
+              className=" text-2xl font-bold"
+              initial={{ x: "-300px" }}
+              animate={isSkillRefInView ? { x: 0 } : {}}
+              transition={{ delay: 0.2 }}
+            >
+              SKILLS
+            </motion.h1>
             {/* SKILL LIST */}
-            <div className=" flex flex-wrap gap-4 ">
+            <motion.div
+              className=" flex flex-wrap gap-4 "
+              initial={{ x: "-300px" }}
+              animate={isSkillRefInView ? { x: 0 } : {}}
+            >
               <div className=" cursor-pointer rounded bg-black p-2 text-sm text-white hover:bg-white hover:text-black">
                 Javascript
               </div>
@@ -131,9 +172,9 @@ const AboutPage = () => {
               <div className="cursor-pointer  rounded bg-black p-2 text-sm text-white hover:bg-white hover:text-black">
                 Figma
               </div>
-            </div>
+            </motion.div>
             {/* SKILL SCROLL SVG */}
-            <svg
+            <motion.svg
               initial={{ opacity: 0.2, y: 0 }}
               animate={{ opacity: 1, y: "10px" }}
               transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
@@ -154,15 +195,29 @@ const AboutPage = () => {
                 stroke="#000000"
                 strokeWidth="1"
               ></path>
-            </svg>
+            </motion.svg>
           </div>
           {/* EXPERIENCE CONTAINER */}
-          <div className=" flex flex-col justify-center gap-12 pb-48">
+          <div
+            className=" flex flex-col justify-center gap-12 pb-48"
+            ref={experienceRef}
+          >
             {/* EXPERIENCE TITLE */}
-            <h1 className=" text-2xl font-bold">EXPERIENCE</h1>
+            <motion.h1
+              className=" text-2xl font-bold"
+              initial={{ x: "-300px" }}
+              animate={isExperienceInView ? { x: 0 } : {}}
+              transition={{ delay: 0.2 }}
+            >
+              EXPERIENCE
+            </motion.h1>
 
             {/* EXPERIENCE LIST */}
-            <div className="">
+            <motion.div
+              className=""
+              initial={{ x: "-300px" }}
+              animate={isExperienceInView ? { x: 0 } : {}}
+            >
               {/* EXPERIENCE LIST ITEM */}
               <div className=" flex h-48 justify-between">
                 {/* LEFT */}
@@ -264,7 +319,7 @@ const AboutPage = () => {
                 {/* RIGHT */}
                 <div className="w-1/3"></div>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
         {/* SVG CONTAINER */}
