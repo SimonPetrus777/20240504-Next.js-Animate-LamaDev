32afdde0ddb454bcd9a5e1958bd9f4b735f72fba
Simon Petrus <simon.petrus.jkt@gmail.com>
2024-05-11 17:04:48 +0700

    1:52:33

--- src/app/portofolio/page.jsx
+++ src/app/portofolio/page.jsx
@@ -1,7 +1,12 @@
 "use client";
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 const PortofolioPage = () => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({ target: ref });
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
     <motion.div
       className=" h-full"
@@ -9,7 +14,14 @@ const PortofolioPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      Portofolio
+      <div className="relative h-[600vh]" ref={ref}>
+        <div className=" flex h-[calc(100vh-6rem)] w-screen items-center justify-center text-center text-8xl">
+          My Works
+        </div>
+
+        {/* akan top-0 dari parentnya, karena parentnya ada "relative" */}
+        <div className="sticky top-0 flex h-screen items-center gap-4"></div>
+      </div>
     </motion.div>
   );
 };
