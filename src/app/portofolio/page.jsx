e813da03713d75e8fe4c7fb7e37f62f846844238
Simon Petrus <simon.petrus.jkt@gmail.com>
2024-05-12 11:34:21 +0700

    2:05:04 kelar animasi lingkaran

--- src/app/portofolio/page.jsx
+++ src/app/portofolio/page.jsx
@@ -1,11 +1,48 @@
 "use client";
 import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
 import { useRef } from "react";
+
+const items = [
+  {
+    id: 1,
+    color: "from-red-300 to-blue-300",
+    title: "React Commerce",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "/",
+  },
+  {
+    id: 2,
+    color: "from-blue-300 to-violet-300",
+    title: "Next.js Medium Blog",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "/",
+  },
+  {
+    id: 3,
+    color: "from-violet-300 to-purple-300",
+    title: "Vanilla Book App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    link: "/",
+  },
+  {
+    id: 4,
+    color: "from-purple-300 to-red-300",
+    title: "Spotify Music App",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    link: "/",
+  },
+];
 
 const PortofolioPage = () => {
   const ref = useRef();
   const { scrollYProgress } = useScroll({ target: ref });
-  const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const x = useTransform(scrollYProgress, [0, 1], ["0%", "-80%"]);
 
   return (
     <motion.div
@@ -20,7 +57,61 @@ const PortofolioPage = () => {
         </div>
 
         {/* akan top-0 dari parentnya, karena parentnya ada "relative" */}
-        <div className="sticky top-0 flex h-screen items-center gap-4"></div>
+        <div className="sticky top-0 flex h-screen items-center gap-4 overflow-hidden">
+          <motion.div style={{ x }} className="flex">
+            {/* div kosongan agar konten items yang pertama masih bisa kelihatan ketika udah scroll dari atas halaman */}
+            <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-r from-purple-300 to-red-300" />
+
+            {items.map((item) => (
+              <div
+                className={`flex h-screen w-screen items-center justify-center bg-gradient-to-r ${item.color} `}
+                key={item.id}
+              >
+                <div className=" flex flex-col gap-8 text-white">
+                  <h1>{item.title}</h1>
+                  <div className="relative">
+                    <Image src={item.img} alt="" fill />
+                  </div>
+                  <p>{item.desc}</p>
+                  <Link href={item.link}>
+                    <button>See Demo</button>
+                  </Link>
+                </div>
+              </div>
+            ))}
+          </motion.div>
+        </div>
+      </div>
+      <div className=" flex h-screen w-screen flex-col items-center justify-center gap-16 text-center">
+        <h1 className=" text-8xl">Do you have a project?</h1>
+        <div className="relative">
+          <motion.svg
+            viewBox="0 0 300 300"
+            className=" h-64 w-64 md:h-[500px] md:w-[500px]"
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+              />
+            </defs>
+            <text fill="#000">
+              <textPath xlinkHref="#circlePath" className="text-xl">
+                Front-end Developer and UI Designer
+              </textPath>
+            </text>
+          </motion.svg>
+
+          {/* kita buat link textnya ada di pas tengah2 buletan */}
+          <Link
+            href="/contact"
+            className=" absolute bottom-0 left-0 right-0 top-0 m-auto flex h-16 w-16 items-center justify-center rounded-full bg-black text-white md:h-28 md:w-28"
+          >
+            Hire Me
+          </Link>
+        </div>
       </div>
     </motion.div>
   );
