02266af0b15d265a119a90bb1b7408db8442840b
Simon Petrus <simon.petrus.jkt@gmail.com>
2024-05-12 15:32:50 +0700

    Projeg Kelarrzzz

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,43 @@
 "use client";
 import { motion } from "framer-motion";
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactPage = () => {
+  const [success, setSucccess] = useState(false);
+  const [error, setError] = useState(false);
+
   const text = "Halo Bosque";
+
+  // EMAILJS
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    // setiap nyoba kirim email, kudu reset state dulu
+    setSucccess(false);
+    setError(false);
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_SERVICE_ID,
+        process.env.NEXT_PUBLIC_TEMPLATE_ID,
+        form.current,
+        {
+          publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY,
+        },
+      )
+      .then(
+        () => {
+          setSucccess(true);
+          form.current.reset(); // untuk reset form setelah sukses kirim email
+        },
+        (error) => {
+          setError(true);
+        },
+      );
+  };
 
   return (
     <motion.div
@@ -34,7 +69,38 @@ const ContactPage = () => {
         </div>
 
         {/* FORM CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2">B</div>
+        <form
+          onSubmit={sendEmail}
+          ref={form}
+          className="flex h-1/2 flex-col  justify-center gap-8 rounded-xl bg-red-50 p-24 text-xl lg:h-full lg:w-1/2"
+        >
+          <span>Dear Lama Dev,</span>
+          <textarea
+            rows={6}
+            className=" resize-none border-b-2 border-b-black bg-transparent outline-none"
+            name="user_message"
+          />
+          <span>My email address</span>
+          <input
+            type="text"
+            className=" border-b-2 border-b-black bg-transparent outline-none"
+            name="user_email"
+          />
+          <span>Regards</span>
+          <button className=" rounded bg-purple-200 p-4 font-semibold text-gray-600">
+            Send
+          </button>
+          {success && (
+            <span className=" font-semibold text-green-600">
+              Your message has been sent successfully!
+            </span>
+          )}
+          {error && (
+            <span className=" font-semibold text-red-600">
+              Something went wrong!
+            </span>
+          )}
+        </form>
       </div>
     </motion.div>
   );
