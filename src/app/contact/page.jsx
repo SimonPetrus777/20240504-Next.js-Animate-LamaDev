00a9dbc540e334f5f59622968e09081cf2a1c215
Simon Petrus <simon.petrus.jkt@gmail.com>
2024-05-12 12:50:36 +0700

    2:13:33

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,6 +2,8 @@
 import { motion } from "framer-motion";
 
 const ContactPage = () => {
+  const text = "Halo Bosque";
+
   return (
     <motion.div
       className=" h-full"
@@ -9,7 +11,31 @@ const ContactPage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      ContactPage
+      <div className="flex h-full flex-col px-4 sm:px-8 md:px-12 lg:flex-row lg:px-20 xl:px-48">
+        {/* TEXT CONTAINER */}
+        <div className=" flex h-1/2 items-center justify-center text-6xl lg:h-full lg:w-1/2">
+          <div>
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            😊
+          </div>
+        </div>
+
+        {/* FORM CONTAINER */}
+        <div className="h-1/2 lg:h-full lg:w-1/2">B</div>
+      </div>
     </motion.div>
   );
 };
